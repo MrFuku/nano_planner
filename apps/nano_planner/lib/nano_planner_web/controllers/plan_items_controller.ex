6fcd7394b598357428649988c8c86cd8d68993e0
MrFuku <mmafighter_shun@icloud.com>
2020-09-01 22:24:27 +0900

    controllers修正

--- apps/nano_planner/lib/nano_planner_web/controllers/plan_items_controller.ex
+++ apps/nano_planner/lib/nano_planner_web/controllers/plan_items_controller.ex
@@ -1,13 +1,16 @@
-defmodule NanoPlanner.PlanItemsController do
-  use NanoPlanner.Web, :controller
-  alias NanoPlanner.PlanItem
+defmodule NanoPlannerWeb.PlanItemsController do
+  use NanoPlannerWeb, :controller
+  import Ecto.Query
+  alias NanoPlanner.Repo
+  alias NanoPlanner.Schedule
+  alias NanoPlanner.Schedule.PlanItem
 
   def index(conn, _params) do
     plan_items =
       PlanItem
       |> order_by(asc: :starts_at, asc: :ends_at, asc: :id)
       |> Repo.all
-      |> PlanItem.convert_datetime
+      |> Schedule.convert_datetime
     render conn, "index.html", plan_items: plan_items
   end
 
@@ -15,7 +18,7 @@ defmodule NanoPlanner.PlanItemsController do
     plan_item =
       PlanItem
       |> Repo.get!(params["id"])
-      |> PlanItem.convert_datetime
+      |> Schedule.convert_datetime
     render conn, "show.html", plan_item: plan_item
   end
 end
